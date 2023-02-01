1eb1e34573723d21044678dcb5e5e7364c52a67a
OohBleh <ariasanovsky@users.noreply.github.com>
2023-02-01 06:39:38 -0500

    guess -> u32 and comparisons/matches

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use std::io;
 // using the rand library
 use rand::Rng;
 
+// smart comparisons with enums
+use std::cmp::Ordering;
+
 // no ! : this is a function, not a macro
 fn main() {
 
@@ -15,7 +18,7 @@ fn main() {
         .gen_range(1..=100);
     // second line is calling it with a range
     // if I let oneHundred = 1..=100; then oneHundred is an int range
-    
+
     println!("The secret number is: {secret_number}");
     
     println!("Please input your guess.");
@@ -40,4 +43,20 @@ fn main() {
     let y = 10;
     println!("x = {x} and y + 2 = {}", y + 2);
     */
+
+    // reuses variable name
+    let guess: u32 = guess.trim()
+        .parse()
+        .expect("Please type a number!");
+    // trim removes whitespace on the ends
+    // parse converts to the desired type (u32)
+    // also returns a Return (same as above, Ok or Err)
+    // expect says what to complain about in the Err case
+    
+    // a match has arms (patterns to match)
+    match guess.cmp(&secret_number){
+        Ordering::Less      => println!("Too small!"), 
+        Ordering::Greater   => println!("Too large!"), 
+        Ordering::Equal     => println!("You win!"), 
+    }
 }
